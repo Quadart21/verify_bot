03804d0f9f3dbda1f8e094d0cc7b97b8ff99bc31
Quadart21 <108038976+Quadart21@users.noreply.github.com>
2025-05-25 19:56:20 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-BOT_TOKEN = "7722719066:AAH4cRPPdkbNOggZ8ujPWrJ5tLhjvzPvJvY"
-GROUP_CHAT_ID = -1002250802160  # замените на ваш реальный ID группы
+BOT_TOKEN = "Y"
+GROUP_CHAT_ID = -  # замените на ваш реальный ID группы
 
 # ID операторов Telegram, у которых есть доступ к управлению верификацией
-OPERATORS = [8058997213, 7773331170, 7887465538]
+OPERATORS = []
 
 
